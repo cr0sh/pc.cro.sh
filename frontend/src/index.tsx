d4e4a6f1292307b34b2b4e66a933acd701832c48
Nam Junghyun <junghyun@hyperithm.com>
2021-09-27 23:37:56 +0900

    frontend: Add sentry intergration

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+if (process.env.REACT_APP_SENTRY_DSN !== undefined) {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    integrations: [new Integrations.BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+} else {
+  console.warn("Sentry DSN not configured; skipping init");
+}
 
 ReactDOM.render(
   <React.StrictMode>
